2459c9d9b1f61a0d8f0d10f31aeba3da650e32cd
ivndrkk <ivandereka19@gmail.com>
2025-07-06 14:51:55 +0100

    darktheme

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
     new Splide('#image-carousel').mount();
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('theme-toggle');
+    const body = document.body;
+
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        body.classList.add('dark-theme');
+    }
+
+    toggleButton.addEventListener('click', () => {
+        body.classList.toggle('dark-theme');
+        const isDark = body.classList.contains('dark-theme');
+
+        localStorage.setItem('theme', isDark ? 'dark' : 'light');
+    });
+});
